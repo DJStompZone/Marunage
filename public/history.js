e302782aece06df95c3f8e12d0cbc1e54751c38b
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2016-03-17 10:41:25 +0000

    [Change] - The sequence of history in descending order

--- public/history.js
+++ public/history.js
@@ -12,15 +12,15 @@ class HistoryCtrl {
   constructor($http) {
     $http.get('/api/history/list').success((data) => {
       console.log(data);
-      this.history = data.history;
+      this.history = data.history.reverse();
     });
   }
 }
 
 app.component("history", {
   template: `
-    <figure ng-repeat="url in $ctrl.history">
-      <img src={{url}}>
+    <figure ng-repeat="url in $ctrl.history ">
+      <img lazyload="1" src={{url}}>
       <figcation><a href={{url}} target="_blank">{{url}}</a></figcation>
     </figure>
   `,
