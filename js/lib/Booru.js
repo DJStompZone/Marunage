d14d08cdb52dae0c544f92d65c5d34ea60a62f0d
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2018-06-17 12:29:43 +0900

    [Implement] - request job queue(interval: 5000 ms, retry: 3 times)

--- js/lib/Booru.js
+++ js/lib/Booru.js
@@ -1,34 +1,67 @@
-const Browser = require('./Browser');
+const fsp = require('fs').promises;
+const path = require('path');
+const download = require('download');
+const Requester = require(path.resolve('js', 'lib', 'Requester'));
 
-module.exports = class Booru extends Browser {
+module.exports = class Booru {
   constructor({ resizeOptions }) {
-    super({ url: 'https://waifu2x.booru.pics/' });
+    this.endpoint = 'https://waifu2x.booru.pics/';
     this.resizeOptions = resizeOptions;
   }
 
-  /**
-   * puppetterのlaunchやgoto処理はすべて親クラスで行う。ページアクセス後の処理をこの関数内に記述してtask関数として渡す。
-   */
   async download() {
-    const task = async ({ page }) => {
-      console.log('this.resizeOptions => ', this.resizeOptions);
-      const tabs = await page.$$('.nav.nav-tabs li');
-      await tabs[1].click();
-      await page.type('#url input[name="url"]', this.resizeOptions.url);
-      await page.click(
-        '#url input[name="denoise"]',
-        this.resizeOptions.denoise,
-      );
-      await page.click('#url input[name="scale"]', this.resizeOptions.scale);
-      await page.click('#url #submit');
-      await page.waitForNavigation({ waitUntil: 'networkidle0' }),
-        await page.waitForNavigation({
-          timeout: 1000 * 15,
-          waitUntil: 'domcontentloaded',
-        });
-    };
+    const response = await new Requester({ params: this.resizeOptions }).get();
+    console.log(response.status);
+    console.log(response.data);
 
-    await this.go({ task });
+    const foundByExisting = response.data.match(
+      '<a href="/outfiles/(.*).jpg" class="btn btn-default">',
+    );
+    const textBySplited = response.data.split(' ');
+
+    let hash = null;
+
+    // 生成済み
+    if (foundByExisting !== null) {
+      hash = foundByExisting[1];
+    } else {
+      // 生成中
+      // ex) $.getJSON("\/Home\/statusjson?" + "handle=" + "H:waifu2x.slayerduck.com:913096"  + "&hash=" + "734c1a8558be03134c9c71f4692bb0f9c5f5914d_s2_n2", function(data) {
+      const hashIndex = textBySplited.indexOf('"&hash="');
+      if (hashIndex !== -1) {
+        // "f9d405763a54c7925c135206545f5717b899c8bc_s2_n2", の形で抽出されるので余計な部分は取り除く
+        hash = textBySplited[hashIndex + 2].replace(/"|,/g, '');
+      }
+    }
+
+    console.log('\nhash ==> ', hash);
+
+    if (hash === null || hash === '') {
+      throw new Error('failed to resize in booru2x');
+    }
+
+    const ext = '.jpg'; // TODO: 選択式
+    const mine = ext === '.png' ? 'image/png' : 'image/jpeg';
+    const dist = path.resolve('public', 'images');
+    const filename = `${hash}${ext}`;
+    const filepath = path.resolve(dist, filename);
+    console.log('\ndownload ==> ', filename);
+
+    const data = await download(
+      `https://waifu2x.booru.pics/outfiles/${filename}`,
+      { timeout: 1000 * 30 },
+    );
+
+    console.log('\nsave => ', filepath);
+
+    const _ = await fsp.writeFile(filepath, data);
+
+    console.log('Booru proceed', data, filename);
+
+    return {
+      data,
+      filename,
+    };
   }
 
   format() {
--- js/routes/booru.js
+++ js/routes/booru.js
@@ -1,14 +1,12 @@
-const fsp = require('fs').promises;
 const path = require('path');
-const download = require('download');
 const configs = require('konfig')();
 const Booru = require(path.resolve('js', 'lib', 'Booru'));
-const Requester = require(path.resolve('js', 'lib', 'Requester'));
+const jobQueue = require(path.resolve('js', 'lib', 'JobQueueManager'));
 const GMailer = require(path.resolve('js', 'lib', 'GMailer'));
 const HistoryProvider = require(path.resolve('js', 'model', 'HistoryProvider'));
 
 /**
- * TOOD: 個々の処理をクラス化
+ *
  * @param {*} app
  */
 module.exports = app => {
@@ -23,59 +21,51 @@ module.exports = app => {
       denoise: req.body.noise - 0,
       scale: req.body.scale - 0,
     };
+    const booru = new Booru({ resizeOptions });
 
     try {
-      const response = await new Requester({ params: resizeOptions }).get();
-      console.log(response.status);
-      console.log(response.data);
-
-      const foundByExisting = response.data.match(
-        '<a href="/outfiles/(.*).jpg" class="btn btn-default">',
-      );
-      const textBySplited = response.data.split(' ');
-
-      let hash = null;
-
-      // 生成済み
-      if (foundByExisting !== null) {
-        hash = foundByExisting[1];
-      } else {
-        // 生成中
-        // ex) $.getJSON("\/Home\/statusjson?" + "handle=" + "H:waifu2x.slayerduck.com:913096"  + "&hash=" + "734c1a8558be03134c9c71f4692bb0f9c5f5914d_s2_n2", function(data) {
-        const hashIndex = textBySplited.indexOf('"&hash="');
-        if (hashIndex !== -1) {
-          // "f9d405763a54c7925c135206545f5717b899c8bc_s2_n2", の形で抽出されるので余計な部分は取り除く
-          hash = textBySplited[hashIndex + 2].replace(/"|,/g, '');
-        }
-      }
-
-      if (hash === null || hash === '') {
-        throw new Error('failed to resize in booru2x');
-      }
-
-      const ext = '.png'; // TODO: 選択式
-      const mine = ext === '.png' ? 'image/png' : 'image/jpeg';
-      const dist = path.resolve('public', 'images');
-      const filename = `${hash}${ext}`;
-      const filepath = path.resolve(dist, filename);
-      const data = await download(
-        `https://waifu2x.booru.pics/outfiles/${filename}`,
-        { timeout: 1000 * 120 },
-      );
-      const _ = await fsp.writeFile(filepath, data);
-
+      const { data, filename } = await booru.download();
       return res.json({
         data: data,
         filename: filename,
       });
     } catch (error) {
       console.error('error => ', error);
-      console.error('error.data => ', error.data);
-      // new Gmailer().send(error, req.body);
-      res.status(404).send({
-        body: req.body,
-        error: error,
-      });
+      console.error('error.statusCode => ', error.statusCode);
+
+      // error.
+      if (error.statusCode === 404) {
+        console.log('enquque ->\n\n');
+        jobQueue.register({
+          name: 'resize transporting',
+          process: async job => await booru.download(),
+          completed: (job, result) => {
+            console.log('[resize] complete =>\n ', job.id, job.data, result);
+            return res.json({
+              data: result.data,
+              filename: result.filename,
+            });
+          },
+          failed: (job, result) => {
+            console.log('[resize] error => \n ', job.id, job.data, result);
+            return res.status(500).send({
+              body: req.body,
+              error: result,
+            });
+          },
+        });
+        jobQueue.add({
+          name: 'resize transporting',
+          opts: { backoff: 5000, attempts: 4 },
+        });
+      } else {
+        console.log('\n\nもうあかんわ ->');
+        // new Gmailer().send(error, req.body);
+        res.status(500).send({
+          body: req.body,
+          error: error,
+        });
+      }
     }
   });
 };
