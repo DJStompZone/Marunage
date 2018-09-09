081b5d0d3efbd82f6eb2b9c418c8c10881b3abd1
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2018-09-10 01:55:56 +0900

    fix layout

--- public/history.js
+++ public/history.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
-const app = angular.module("app", []);
+const app = angular.module('app', []);
 
-app.component("myApp", {
+app.component('myApp', {
   template: `<history></history>`,
   controller: function() {
     this.$onInit = function() {
@@ -11,65 +11,52 @@ app.component("myApp", {
   },
 });
 
-// app.component('menu', {
-//   template: `
-//     <button ng-click="$ctrl.next();"></button>
-//   `,
-//   controller: function() {
-//     this.$onInit = () => {
-//       this.page = 1;
-//     };
-//     this.next = () => {
-//       this.page++;
-//     }
-//   }
-// })
-
-// app.component("scrollable", {
-//   template: '<scrollable></scrollable>',
-//   controller: function() {
-
-//   }
-// })
-
-app.component("history", {
+app.component('history', {
   template: `
-    <figure ng-repeat="history in $ctrl.histories track by $index">
-      <img lazyload="1" src={{history.url}}>
-      <figcation><a href={{history.url}} target="_blank">{{history.url}}</a></figcation>
-    </figure>
-    <section class="menu">
-      <button class="button" ng-disabled="$ctrl.isLast" ng-click="$ctrl.next();">次の20件</button>
+    <section class="history__container">
+      <figure ng-repeat="history in $ctrl.histories track by $index">
+        <img lazyload="1" src={{history.url}}>
+        <figcation><a href={{history.url}} target="_blank">{{history.url}}</a></figcation>
+      </figure>
+      <section class="menu">
+        <button class="button button-expand" ng-disabled="$ctrl.isLast" ng-click="$ctrl.next();">次の20件</button>
+      </section>
     </section>
   `,
-  controller: HistoryCtrl
+  controller: HistoryCtrl,
 });
 
-function HistoryCtrl ($http) {
+function HistoryCtrl($http) {
   this.$onInit = () => {
     this.page = 0;
     this.limit = 20;
     this.isLast = false;
     this.histories = [];
-    this.fetchHistory(this.createQueryString({page: this.page, limit: this.limit}));
+    this.fetchHistory(
+      this.createQueryString({ page: this.page, limit: this.limit }),
+    );
   };
-  this.createQueryString = (json) => {
-    return Object.keys(json).map( (key) => `${encodeURIComponent(key)}=${encodeURIComponent(json[key])}` ).join('&');
+  this.createQueryString = json => {
+    return Object.keys(json)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(json[key])}`)
+      .join('&');
   };
-  this.fetchHistory = (qs) => {
-    $http.get(`/api/history/list?${qs}`).success((data) => {
-      if(!data.history.length) {
+  this.fetchHistory = qs => {
+    $http.get(`/api/history/list?${qs}`).success(data => {
+      if (!data.history.length) {
         this.isLast = true;
         return;
       }
       this.histories.concat(data.history);
-      data.history.map( history => this.histories.push(history) );
+      data.history.map(history => this.histories.push(history));
     });
-  }
+  };
   this.next = () => {
     this.page++;
-    this.fetchHistory(this.createQueryString({page: this.page, limit: this.limit}));
-  }
+    this.fetchHistory(
+      this.createQueryString({ page: this.page, limit: this.limit }),
+    );
+  };
 }
 
 angular.bootstrap(document.body, [app.name]);
